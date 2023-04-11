4f5a0e06d224ad3f2e86cdc8ed26c7d0d992e16c
Marina Ramiro <in1case@gmail.com>
2023-04-11 20:41:30 +0200

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print('Hello World')
-# El en working directory tenemos un fichero
 
 #git init
 #git add .
 #git commit -m "first commit"
-#git config --global user.name "Marina Ramiro"
+#git status
+#git checkout -- main.py
