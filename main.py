cbd64620d38bfebc07fa7e97aa5c1ab60c3588b9
Marina Ramiro <in1case@gmail.com>
2023-04-11 20:51:46 +0200

    third commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 print('Hello World')
 
-#git init
-#git add .
-#git commit -m "first commit"
+#git init       para iniciar Git
+#git add .      para añadir el fichero en el repositorio local
+#git commit -m "first commit"       para pasar del repositorio local al staging area
 #git status
 #git checkout -- main.py
+
+print('New Version')
+
+#git log        para poder ver todos los commits que hemos hecho
